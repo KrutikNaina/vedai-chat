63066f4b6b1dba8da02dac65d7ee92937d0b07c1
Krutik Naina <nenakrutik2004@gmail.com>
2025-10-02 21:30:54 +0530

    Add Manualy key in  seo page

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -6,19 +6,27 @@ const SEO = ({ title, description, keywords, url, image }) => {
       <title>{title}</title>
       <meta name="description" content={description} />
       <meta name="keywords" content={keywords} />
-      <meta
-        name="google-site-verification"
-        content={import.meta.env.VITE_GSC_KEY}
-      />
+
+      {/* Open Graph / Social */}
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
       <meta property="og:type" content="website" />
       <meta property="og:url" content={url} />
       <meta property="og:image" content={image} />
+
+      {/* Twitter Card */}
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
       <meta name="twitter:image" content={image} />
+
+      {/* Google Search Console verification */}
+      <meta
+        name="google-site-verification"
+        content="v4HAo6qLysy3J6jnJvDOn81xHBqZTZpoxs2ssYGMCRg"
+      />
+
+      {/* JSON-LD Structured Data */}
       <script type="application/ld+json">
         {JSON.stringify({
           "@context": "https://schema.org",
